29c54d592bf75a4ccbf76c080fe06a49b8017853
Md Masoom <mdmasoom9060@gmail.com>
2025-03-20 03:34:37 +0530

    add dowload button

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 function Resume() {
+  const onButtonClick = () => {
+    const pdfUrl = "Md_Masoom_New_Resume.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "masoom_resume.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
   return (
     <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-8" onClick={onButtonClick}>
+                    Download PDF
+                </button>
       <iframe
         src="Md_Masoom_New_Resume.pdf"
         frameborder="0" title="Resume" 
         className="h-screen w-[100%]"
       ></iframe>
+      
     </div>
   );
 }
